8b7a7f4482b94927aae14accd0fb377f93282562
Sabiq Khan <sabiq.am.khan@gmail.com>
2023-05-14 12:25:50 -0700

    Implemented link creation and error handling
    	binadd.sh:
    		* Found that `ln` can only create links once, but it
    		  only needs to create the link once
    		* Any future changes will be reflected in the link
    		* Added the link functionality and progress messages
    		  that get printed to stdout
    		* Added error handling
    		* Updated TODO

--- binadd.sh
+++ binadd.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# This code is successfully able to check & print name of the
-# current branch
-# Tested by creating a new branch and running script
-# TODO: Print to stderr
-# TODO: Change the "main" to "master" when the script works
+# TODO: Consider splitting into functions
 currBranch=$(git rev-parse --abbrev-ref HEAD)
-if ! [ $currBranch = "main" ]; then
-	echo "ERROR: Current branch is $currBranch, not main." >&2 
+if ! [ $currBranch = "bin-add" ]; then
+	echo "ERROR: Current branch is $currBranch, not bin-add." >&2 
 	exit 1
 fi
 
-echo "Current branch is main."
-echo "$PWD/*"
+echo "Current branch is bin-add."
+set -o errtrace
+trap "echo ERROR: Link creation failed." ERR
+scripts=$(ls $PWD/*)
+for script in $scripts; do
+	link=$HOME/bin/$(basename $script | sed 's/\..*//g')
+	echo "Creating link in $HOME/bin..."
+	ln -s $script $link
+	if [ $? -eq 0 ]; then
+		echo "Link $link created."
+	fi
+done
+
