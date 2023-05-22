78f18aadf88b2ebf96ad62e3225c99a28ef5d72e
Sabiq Khan <sabiq.am.khan@gmail.com>
2023-05-21 19:59:48 -0700

    Changed scope of function variables to local
    	binlink.sh:
    		* Changed the scope of variables in functions such as
    		  `$matches` in `$search()` and `$scripts` and `$link`
    in `add()` to local
    		* Did this because locally scoping variables in
    		  functions is generally a best practice
    		* Tested `search()`, after changing the logic of a
    		  certain conditional, I was able to get it to work
    		* Tested `add()` as well and it works the same without
    		  the need for any additional changes

--- binlink.sh
+++ binlink.sh
@@ -15,7 +15,7 @@ add() {
 	fi
 
 	# Checks that the current branch is master
-	currBranch=$(git rev-parse --abbrev-ref HEAD)
+	local currBranch=$(git rev-parse --abbrev-ref HEAD)
 	if ! [ $currBranch = "master" ]; then
 		echo "ERROR: Current branch is $currBranch, not master." >&2 
 		exit 1
@@ -25,9 +25,9 @@ add() {
 	# If link already exists, an error is returned
 	set -o errtrace
 	trap "echo ERROR: Link creation failed." ERR
-	scripts=$(ls $PWD/*)
+	local scripts=$(ls $PWD/*)
 	for script in $scripts; do
-		link=$HOME/bin/$(basename $script | sed 's/\..*//g')
+		local link=$HOME/bin/$(basename $script | sed 's/\..*//g')
 		echo "Creating link in $HOME/bin..."
 		ln -s $script $link
 		if [ $? -eq 0 ]; then
@@ -49,8 +49,8 @@ list(){
 
 # Searches ~/bin for a specified link
 search(){
-	matches=$(ls $HOME/bin | grep $1)
-	if [ $? -eq 1 ]; then
+	local matches=$(ls $HOME/bin | grep $1)
+	if [[ -z $matches ]]; then
 		echo "ERROR: No symlinks found in $HOME/bin like '$1'." >&2
 	else
 		local heading=$(header "Symlinks like '$1' in $HOME/bin:")
